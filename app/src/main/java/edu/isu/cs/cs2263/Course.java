b833b29c990ff2ccf333dd50a4395606dd295156
pratshow <pratshow@isu.edu>
2021-10-03 16:38:24 -0600

    Student, class, and UI support

--- app/src/main/java/edu/isu/cs/cs2263/Course.java
+++ app/src/main/java/edu/isu/cs/cs2263/Course.java
@@ -1,4 +1,45 @@
 package edu.isu.cs.cs2263;
 
+/**
+ * Class to manage the classes that students take
+ */
 public class Course {
+    private Integer number;
+    private String subject;
+    private String title;
+
+    /**
+     * @param number the course number
+     * @param subject type or department of course
+     * @param title name of the course
+     */
+    public Course(Integer number, String subject, String title) {
+        this.number = number;
+        this.subject = subject;
+        this.title = title;
+    }
+    /**
+     * @param num change the course number
+     */
+    public void setNumber(Integer num){ number = num; }
+    /**
+     * @param subj change the class subject/department name
+     */
+    public void setSubject(String subj){ subject = subj; }
+    /**
+     * @param title change the name of the course
+     */
+    public void setTitle(String title){ this.title = title; }
+    /**
+     * @return course number
+     */
+    public Integer getNumber(){ return number; }
+    /**
+     * @return subject/department name
+     */
+    public String getSubject(){ return subject; }
+    /**
+     * @return name of the course
+     */
+    public String getTitle(){ return title; }
 }
--- app/src/main/java/edu/isu/cs/cs2263/Student.java
+++ app/src/main/java/edu/isu/cs/cs2263/Student.java
@@ -1,32 +1,68 @@
 package edu.isu.cs.cs2263;
 
-public class Student {
-    String firstName;
-    String lastName;
+import java.util.*;
 
+/**
+ * Class to manage all the students and what classes they are taking.
+ */
+public class Student {
+    private String firstName;
+    private String lastName;
+    List<Course> courseList;
+
+    /**
+     * Makes a Student object with a full name and an empty class list.
+     * @param firstName the student's first name
+     * @param lastName the student's last name
+     */
     public Student(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.courseList = new ArrayList<>();
     }
 
-    private void setFirstName(String name){
-        firstName = name;
-    }
+    /**
+     * @param name students first name
+     */
+    public void setFirstName(String name){ firstName = name; }
 
-    private void setLastName(String name){
-        lastName = name;
-    }
+    /**
+     * @param name students last name
+     */
+    public void setLastName(String name){ lastName = name; }
 
-    public String getFirstName(){
-        return firstName;
-    }
+    /**
+     * @return the student's first name
+     */
+    public String getFirstName(){ return firstName; }
 
-    public String getLastName(){
-        return lastName;
-    }
+    /**
+     * @return student's last name
+     */
+    public String getLastName(){ return lastName; }
 
-    public String toString(){
+    /**
+     * @return the list of courses that the student is enrolled in.
+     */
+    public List<Course> getCourseList(){ return courseList; }
+
+    /**
+     * Creates a string for the first and last name of the student and all of the classes they are enrolled in.
+     * @return a string with the students name and list of classes
+     */
+    public void makeString(){
         String fullName = (firstName + " " + lastName);
-        return fullName;
+        String className = null;
+        for (Course course: courseList) {
+            className = (className + "\n" + course.getTitle());
+        }
+        System.out.println("The students name is " + fullName + "\n" + "they are enrolled in" + className);
     }
+
+    /**
+     * Adds a course to a students course list.
+     * @param course Course the student would like to enroll in.
+     */
+    public void addCourse(Course course){ courseList.add(course); }
+
 }
